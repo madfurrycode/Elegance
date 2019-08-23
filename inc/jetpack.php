96e8ec1950be87dc34bff250b42b767129c2270c
Tristan <tristandevelliott1996@gmail.com>
2019-08-22 17:41:45 -0700

    Project init
    Initializing the project.

---
+++ inc/jetpack.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ *
+ * @link https://jetpack.com/
+ *
+ * @package Elegance
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ * See: https://jetpack.com/support/content-options/
+ */
+function elegance_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'elegance_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+
+	// Add theme support for Content Options.
+	add_theme_support( 'jetpack-content-options', array(
+		'post-details'    => array(
+			'stylesheet' => 'elegance-style',
+			'date'       => '.posted-on',
+			'categories' => '.cat-links',
+			'tags'       => '.tags-links',
+			'author'     => '.byline',
+			'comment'    => '.comments-link',
+		),
+		'featured-images' => array(
+			'archive'    => true,
+			'post'       => true,
+			'page'       => true,
+		),
+	) );
+}
+add_action( 'after_setup_theme', 'elegance_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function elegance_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_type() );
+		endif;
+	}
+}
---
+++ inc/setup/Enqueue.php
@@ -0,0 +1,36 @@
+<?php 
+/**
+ * Elegance Enqueue Stylesheets & Scripts
+ * @package Elegance
+ * @author Tristan Elliott
+ * 
+ * ------
+ * Visit for more information
+ * https://developer.wordpress.org/themes/basics/including-css-javascript/
+ * 
+ */
+
+function elegance_scripts()
+{
+    /**
+     * Stylesheet Enqueue
+     * @version 1.0.0
+     */
+    wp_enqueue_style( 'elegance-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/assets/dist/css/bootstrap.min.css', array(), '4.3.1', 'all');
+    wp_enqueue_style( 'font-awesome-css', get_template_directory_uri() . '/assets/dist/fonts/font-awesome.min.css', array(), '4.0.0', 'all');
+
+    /**
+     * Scripts Enqueue
+     * @version 1.0.0
+     */
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/dist/js/bootstrap.min.js', array( 'jquery' ), '4.3.1', true );
+
+    /**
+     * Woocommerce Stylesheet
+     * @version 1.0.0
+     */
+    wp_enqueue_style( 'woocommerce-css', get_template_directory_uri() . '/assets/dist/css/woocommerce.css', array(), '1.0.0', 'all');
+    
+}
+add_action( 'wp_enqueue_scripts', 'elegance_scripts' );
